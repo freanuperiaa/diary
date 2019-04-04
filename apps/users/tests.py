638f4b6fc7c398da1f368a2f712b29e40c9e2d98
magnussonfrancis <magnussonfrancis@gmail.com>
2019-04-04 14:57:22 +0800

    added space

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -55,3 +55,4 @@ class ProfileViewTest(TestCase):
         response2 = self.client.get('/users/profile/2/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response2.status_code, 404)
+        
